3ffd44a711aac9de817e513732e245679be92302
Jagdish Arya <98749933+JCArya@users.noreply.github.com>
2022-02-09 16:05:15 +0530

    Add files via upload

---
+++ convert_decimal_to_binary.py
@@ -0,0 +1,8 @@
+# Python program to convert decimal into other number systems
+#dec = 344
+dec=int(input("Enter Decimal"))
+
+print("The decimal value of", dec, "is:")
+print(bin(dec), "in binary.")
+print(oct(dec), "in octal.")
+print(hex(dec), "in hexadecimal.")
