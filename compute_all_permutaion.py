3768f871d6f72f0db1164668256e4e4f02e55eb9
Jagdish Arya <98749933+JCArya@users.noreply.github.com>
2022-02-09 15:33:48 +0530

    Add files via upload

---
+++ compute_all_permutaion.py
@@ -0,0 +1,18 @@
+import time
+def get_permutation(string, i=0):
+
+    if i == len(string):   	 
+        print("".join(string))
+        
+
+    for j in range(i, len(string)):
+
+        words = [c for c in string]
+        time.sleep(1)
+   
+        # swap
+        words[i], words[j] = words[j], words[i]
+   	 
+        get_permutation(words, i + 1)
+
+print(get_permutation('yup'))
